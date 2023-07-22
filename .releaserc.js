d009517f4713eb5051f2d8ce1fa1a9eb19e7b265
Oleksii Kryvets <alex.kryets@gmail.com>
2023-07-22 15:21:03 +0300

    clean up branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    branches: [{ name: "main" }, { name: "next" }],
-    plugins: ["@semantic-release/commit-analyzer"],
+  branches: [{ name: "main" }, { name: "next" }],
+  plugins: ["@semantic-release/commit-analyzer"],
 };
