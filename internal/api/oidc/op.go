7dce7258862871f3a48ccc0d0a7f57006e2f4e98
Oleksii Kryvets <alex.kryets@gmail.com>
2023-12-12 12:19:45 +0200

    fix: form is not parsed before getting data from it

--- internal/api/oidc/op.go
+++ internal/api/oidc/op.go
@@ -130,6 +130,14 @@ func NewServer(
 		userAgentCookie,
 		http_utils.CopyHeadersToContext,
 		accessHandler.HandleIgnorePathPrefixes(ignoredQuotaLimitEndpoint(config.CustomEndpoints)),
+		func(handler http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path == "/oauth/v2/token" {
+					_ = r.ParseMultipartForm(32 << 20)
+				}
+				handler.ServeHTTP(w, r)
+			})
+		},
 	))
 
 	return server, nil
